9df4178cca7cbecf6ac7d07ded1759e1ab2d8410
Laverde <ccclaverde@gmail.com>
2023-12-07 18:58:26 -0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css';
 
+//componets
+
+import Layout from '../components/Layout';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+ <Layout>
+  <Component {...pageProps} />
+  </Layout>
+    
+  );
 }
 
 export default MyApp;

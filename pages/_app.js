b3bacab79aeaa8a9ef849435af6f82c0e5b9f224
Laverde <ccclaverde@gmail.com>
2023-12-08 23:49:12 -0500

    Transition

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,22 @@
 import '../styles/globals.css';
-
 //componets
-
 import Layout from '../components/Layout';
+import  Transition from '../components/Transition';
+//Router
+import { useRouter } from 'next/router';
+import { AnimatePresence, motion } from 'framer-motion';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
- <Layout>
-  <Component {...pageProps} />
-  </Layout>
-    
+    <Layout>
+      <AnimatePresence mode='wait'>
+        <motion.div key={router.route} className='h-full' >
+          <Transition/>
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
+    </Layout>
   );
 }
 

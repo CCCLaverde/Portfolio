a9787b000d70fa77255c13edd524f6810299d10c
Laverde <ccclaverde@gmail.com>
2023-12-11 00:38:09 -0500

    Particles-and-Header

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,34 @@
+import Link from "next/link";
+
+import {
+  RiLinkedinLine,
+  RiGithubLine,
+  RiMailAddLine,
+  RiTwitterLine
+} from 'react-icons/ri';
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+
+      <Link href={'https://github.com/CCCLaverde'} className="hover:text-accent transition-all duration-300">
+        <RiGithubLine/>
+      </Link>
+
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiLinkedinLine/>
+      </Link>
+
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiTwitterLine/>
+      </Link>
+
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiMailAddLine/>
+      </Link>
+
+    </div>
+  )
 };
 
 export default Socials;

ea01d042dea66296cd85fe4a2cbdb1e8e8400fce
Cristian Camilo Castrillón Laverde <125719337+CCCLaverde@users.noreply.github.com>
2023-12-12 05:17:49 +0000

    About-one

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,18 +1,23 @@
+import React, { useState } from "react";
+
+import Avatar from "../../components/Avatar";
+import Circles from "../../components/Circles";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
+
 // icons
 import {
-  FaHtml5,
   FaCss3,
+  FaHtml5,
   FaJs,
-  FaReact,
-  FaWordpress,
-  FaFigma,
+  FaNodeJs,
+  FaReact
 } from "react-icons/fa";
 
 import {
-  SiNextdotjs,
-  SiFramer,
-  SiAdobexd,
-  SiAdobephotoshop,
+  SiNextdotjs
 } from "react-icons/si";
 
 
@@ -29,29 +34,12 @@ const aboutData = [
           <FaJs />,
           <FaReact />,
           <SiNextdotjs />,
-          <SiFramer />,
-          <FaWordpress />,
+          <FaNodeJs/>
         ],
-      },
-      {
-        title: 'UI/UX Design',
-        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
-      },
+      }
     ],
   },
-  {
-    title: 'awards',
-    info: [
-      {
-        title: 'Webby Awards - Honoree',
-        stage: '2011 - 2012',
-      },
-      {
-        title: 'Adobe Design Achievement Awards - Finalist',
-        stage: '2009 - 2010',
-      },
-    ],
-  },
+  
   {
     title: 'experience',
     info: [
@@ -89,7 +77,74 @@ const aboutData = [
 ];
 
 const About = () => {
-  return <div>About</div>;
+  const [ index, setIndex ] = useState(0);
+
+  return (
+    <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
+      <Circles/>
+
+      <motion.div 
+        className="hidden xl:flex absolute bottom-0 -left-[370px]"
+        variants={fadeIn('right', 0.2)}
+        initial="hidden"
+        animate='show'
+        exit='hidden'
+      >
+        <Avatar />
+      </motion.div>
+      <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
+        {/* text */}
+        <div className="flex-1 flex flex-col justify-center">
+          <h2 className="h2">Captivating <span className="text-accent">stories</span> birth magnificent designs.</h2>
+          <p className="max-w-[500] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0"> 
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+          {/* counters */}
+          <div>counters</div>
+        </div>
+        {/* info */}
+        <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+            {aboutData.map((item, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className={ `${
+                    index === itemIndex && 
+                    'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'
+                  } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:absolute after:-bottom-1 after:left-0`}
+                  onClick={() => setIndex(itemIndex)}
+                >
+                  {item.title}
+                </div>
+              )
+            })}
+          </div>
+            <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
+            {aboutData[index].info.map((item, itemIndex) => {
+              return (
+                <div 
+                  key={itemIndex}
+                  className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"  
+                >
+                  {/* titlle */}
+                  <div className="font-light mb-2 md:mb-0">{item.title}</div>
+                  <div className="hidden md:flex">-</div>
+                  <div>{item.stage}</div>
+                  <di className='flex gap-x-4' >
+                    {/* icons */}
+                    {item.icons?.map((icon, itemIndex) => {
+                      return <div className="text-2xl text-white">{icon}</div>
+                    })}
+                  </di>
+                </div>
+              )
+            })}
+            </div>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default About;

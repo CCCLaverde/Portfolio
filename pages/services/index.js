a11107482254d3201317cf6d0f82707a82aadebb
Cristian Camilo Castrillón Laverde <125719337+CCCLaverde@users.noreply.github.com>
2023-12-14 05:52:38 +0000

    Finished-Service

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,11 +1,3 @@
-import {
-
-} from 'react-icons/rx';
-
-export const seviceData = [
-
-]
-
 import ServiceSlider from '../../components/ServiceSlider';
 import Bulb from '../../components/Bulb';
 import Circles from '../../components/Circles'; 
@@ -21,15 +13,35 @@ const Services = () => {
         <div>
           {/* text */}
           <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
-            <h2 className='h2 xl:mt-8'>
+            <motion.h2 
+              className='h2 xl:mt-8'
+              variants={fadeIn('up',0.2)}
+              initial='hidden'
+              animate='show'
+              exit='heidden'
+              >
               My services <span className='text-accent'>.</span>
-              </h2>
-              <p className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
+              </motion.h2>
+              <motion.p 
+                variants={fadeIn('up',0.4)}
+                initial='hidden'
+                animate='show'
+                exit='heidden'
+                className='mb-4 max-w-[400px] mx-auto lg:mx-0'
+                >
                 I am a full-stack developer and I am proficient in a wide range of technologies. I can help you with your next project.
-              </p>
+              </motion.p>
           </div>
           {/* slider */}
+          <motion.div
+           className='w-full xl:max-w-[65%]'
+           variants={fadeIn('down',0.6)}
+           initial='hidden'
+           animate='show'
+           exit='heidden'
+           >
           <ServiceSlider/>
+          </motion.div>
         </div>
       </div>
       <Bulb/>
